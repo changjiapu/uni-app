b90db605ec259d5d8544c3b28c62fba40dd4ef7f
changjiapu <45961167+changjiapu@users.noreply.github.com>
2018-12-25 17:34:36 +0800

    Update config.js

--- common/utils/config.js
+++ common/utils/config.js
@@ -1,5 +1,5 @@
-const appid = 'wx78710d8e515d0116'
-const baseURL = 'https://admin.sinlu.net'  // 图标地址
-const requestURL = 'https://admin.sinlu.net' // ajax请求地址
+const appid =
+const baseURl=
+const requestURL = 
 
 export { appid, baseURL, requestURL }
