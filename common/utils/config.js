4677ec928377d1d85dffdbe665808fc66a4874a5
changjiapu <275298019@qq.com>
2018-12-25 18:41:03 +0800

    统一域名配制位置

--- common/utils/config.js
+++ common/utils/config.js
@@ -1,5 +1,5 @@
-const appid = 'wx78710d8e515d0116'
-const baseURL = 'https://admin.sinlu.net'  // 图标地址
-const requestURL = 'https://admin.sinlu.net' // ajax请求地址
+const appid = ''
+const baseURL = ''  // 图标地址
+const requestURL = '' // ajax请求地址
 
 export { appid, baseURL, requestURL }
