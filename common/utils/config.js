1e1f00e3f04d2df97850f1b021217611f257a761
changjiapu <275298019@qq.com>
2018-12-27 10:15:42 +0800

    添加了我的团队，累计收益

--- common/utils/config.js
+++ common/utils/config.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 const appid = ''
 const baseURL = ''  // 图标地址
 const requestURL = '' // ajax请求地址
-=======
-const appid =
-const baseURl=
-const requestURL = 
->>>>>>> b90db605ec259d5d8544c3b28c62fba40dd4ef7f
 
 export { appid, baseURL, requestURL }
