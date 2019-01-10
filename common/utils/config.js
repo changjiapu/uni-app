70b4a3b97c79d72b72b0c62cf204cd2363a3255a
changjiapu <45961167+changjiapu@users.noreply.github.com>
2019-01-10 15:49:29 +0800

    Update config.js

--- common/utils/config.js
+++ common/utils/config.js
@@ -1,5 +1,5 @@
-const appid ='wx1bde03b0c05764d1'
-const baseURL = 'https://kz.smallcode.net'  // 图标地址
-const requestURL = 'https://kz.smallcode.net' // ajax请求地址
+const appid ='xxx'
+const baseURL = 'xxxx'  // 图标地址
+const requestURL = 'xxxx' // ajax请求地址
 
 export { appid, baseURL, requestURL }
